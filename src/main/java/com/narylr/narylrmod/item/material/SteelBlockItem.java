57f6503000184ab470a47f331eb975bdce3f55ba
Narylr <3377936310@qq.com>
2026-06-08 10:55:25 +0800

    mark steel material items as heavy

--- src/main/java/com/narylr/narylrmod/item/material/SteelBlockItem.java
+++ src/main/java/com/narylr/narylrmod/item/material/SteelBlockItem.java
@@ -1,13 +1,21 @@
 package com.narylr.narylrmod.item.material;
 
+import com.narylr.narylrmod.item.HeavyItem;
 import com.narylr.narylrmod.item.HeavyItemAttributes;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.level.block.Block;
 
-public class SteelBlockItem extends BlockItem {
+public class SteelBlockItem extends BlockItem implements HeavyItem {
     public SteelBlockItem(Block block, Properties properties) {
         super(block, properties);
+    }
+
+    // 钢块本体属于钢块系沉重物品，基础减速 30%
+    @Override
+    public double getHeavyPenalty(ItemStack stack) {
+        return HeavyItemAttributes.STEEL_BLOCK_HEAVY_PENALTY;
     }
 
     // 创建钢块的属性修饰器，让钢块拿在主手时更重，但攻击也更高
